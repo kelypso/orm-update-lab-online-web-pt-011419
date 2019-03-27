0bb39cc0a1d1fac7db73dbd9713f709e8ebffabb
Kelsey Low <kklowlow@gmail.com>
2019-03-27 04:28:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,7 @@ class Student
     sql = <<-SQL
       UPDATE students
       SET name = ?, grade = ?
-      WHERE name = ?
+      WHERE id = ?
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade, self.name)
