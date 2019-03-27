4e48abff8995095d6b59e6afc450bd2e2616dc3b
Kelsey Low <kklowlow@gmail.com>
2019-03-27 04:12:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,21 @@ require_relative "../config/environment.rb"
 class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+
+  
+  
+
+end
+
+
+
+
+
+
+
+
+=begin 
+
   attr_accessor :name, :grade
   attr_writer :id 
   
@@ -68,6 +83,4 @@ class Student
     end.first
   end
   
-  
-
-end
+  =end
