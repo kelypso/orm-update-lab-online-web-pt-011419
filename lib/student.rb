12ef2f2482b937863f86835bf001534bcba34a48
Kelsey Low <kklowlow@gmail.com>
2019-03-27 04:08:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ class Student
     student
   end
   
-  def self.new_from_db(name, grade)
+  def self.new_from_db(row)
     new_student = self.new
     new_student.id = row[0]
     new_student.name =  row[1]
