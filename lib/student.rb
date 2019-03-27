02cc116bcdf18286150056ca5e37dbeb2e678813
Kelsey Low <kklowlow@gmail.com>
2019-03-27 04:17:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,11 +47,14 @@ class Student
     student
   end
  
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-    student
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
   end
+  
  
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
